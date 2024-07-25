dec477b0d4407500b81b7195e93b44e16f64f800
root <hugh.fagan123@gmail.com>
2024-07-25 17:35:52 +1000

    HTML, CSS and JS changes to enable drop down selection for destination to mirror existing amenities submenu, not functional

--- static/js/index.js
+++ static/js/index.js
@@ -3,86 +3,210 @@
 // Note that this is not Object Oriented Programming. It's just the way people used to code JS 10+ years ago.
 // The JS 'object' is nothing more than an associative array (the proper name for a dictionary in Python)
 
+// hbnb = {
+// //     amenitiesInit: function() {
+//         // set up the onclick events for the Amenities radios + button
+//         let amenRadios = document.querySelectorAll("#menu >.contents >.amenities >.choice input[type='radio']");
+//         for (let elem of amenRadios) {
+//             elem.addEventListener("change", function(e) {
+//             let specificSelectedText = document.querySelector("#menu >.contents >.amenities >.title .selected")
+            
+//                 let radioValue = e.target.value
+//                 if (radioValue == 'specific') {
+//                     hbnb.showSpecificAmenitiesSubmenu();
+//                     hbnb.updateSpecificAmenitiesCount();
+//                     specificSelectedText.setAttribute('state', 'show');
+//                 } else {
+//                     // all amenities - empty string
+//                     hbnb.hideSpecificAmenitiesSubmenu()
+//                     specificSelectedText.setAttribute('state', 'hide')
+//                 }
+//             });
+//         }
+
+//         let amenSpecificSelectBtn = document.getElementById("btn-specific-amenities-select");
+//         amenSpecificSelectBtn.addEventListener('click', function() {
+//             hbnb.showSpecificAmenitiesSubmenu()
+
+//             // NOTE: simply clicking the Please Select button won't cause the radio to change
+//             // The button eats up the click event so the label tag + radio won't receive it.
+//             // We'll select the radio if it isn't already selected
+//             if (!amenRadios[1].checked) {
+//                 amenRadios[1].click();
+//             }
+//         });
+
+//         // For the checkboxes in the submenu, let's add events that will update the counter
+//         let selectedAmenitiesCheckboxes = document.querySelectorAll("#amenities-submenu >.items input[type='checkbox']");
+//         for (let c of selectedAmenitiesCheckboxes) {
+//             c.addEventListener('click', function() {
+//                 hbnb.updateSpecificAmenitiesCount();
+//             })
+//         }
+
+//         // Last but not least! Now let's add an event to the OK button in the submenu
+//         // Note that we are just hiding the menu and doing anything anything special
+//         let amenSpecificConfirmBtn = document.getElementById("btn-specific-amenities-ok");
+//         amenSpecificConfirmBtn.addEventListener('click', function(){
+//             hbnb.hideSpecificAmenitiesSubmenu();
+//         })
+
+//     },
+//     showSpecificAmenitiesSubmenu: function() {
+//         // I have set up the CSS in a certain way so that the submenu is shown / hidden
+//         // depending on the 'state' parameter's value in #amenities-submenu
+//         let submenu = document.querySelector("#amenities-submenu")
+//         submenu.setAttribute("state", 'show')
+//     },
+//     hideSpecificAmenitiesSubmenu: function() {
+//         let submenu = document.querySelector("#amenities-submenu")
+//         submenu.setAttribute("state", 'hide')
+//     },
+//     updateSpecificAmenitiesCount: function() {
+//         let specificCount = document.querySelector("#menu >.contents >.amenities >.title .count")
+//         let selectedAmenitiesCheckboxes = document.querySelectorAll("#amenities-submenu >.items input[type='checkbox']");
+
+//         let checkedCount = 0
+//         for (let c of selectedAmenitiesCheckboxes) {
+//             if (c.checked) {
+//                 checkedCount++
+//             }
+//         }
+
+//         specificCount.innerHTML = checkedCount
+//     },
+//     init: function() {
+//         hbnb.amenitiesInit();
+//     }
+// }
+
+
 hbnb = {
     amenitiesInit: function() {
         // set up the onclick events for the Amenities radios + button
-        let amenRadios = document.querySelectorAll("#menu >.contents >.amenities >.choice input[type='radio']");
+        let amenRadios = document.querySelectorAll("#menu > .contents > .amenities > .choice input[type='radio']");
         for (let elem of amenRadios) {
             elem.addEventListener("change", function(e) {
-            let specificSelectedText = document.querySelector("#menu >.contents >.amenities >.title .selected")
-            
-                let radioValue = e.target.value
+                let specificSelectedText = document.querySelector("#menu > .contents > .amenities > .title .selected");
+                let radioValue = e.target.value;
                 if (radioValue == 'specific') {
                     hbnb.showSpecificAmenitiesSubmenu();
                     hbnb.updateSpecificAmenitiesCount();
                     specificSelectedText.setAttribute('state', 'show');
                 } else {
-                    // all amenities - empty string
-                    hbnb.hideSpecificAmenitiesSubmenu()
-                    specificSelectedText.setAttribute('state', 'hide')
+                    hbnb.hideSpecificAmenitiesSubmenu();
+                    specificSelectedText.setAttribute('state', 'hide');
                 }
             });
         }
 
         let amenSpecificSelectBtn = document.getElementById("btn-specific-amenities-select");
         amenSpecificSelectBtn.addEventListener('click', function() {
-            hbnb.showSpecificAmenitiesSubmenu()
-
-            // NOTE: simply clicking the Please Select button won't cause the radio to change
-            // The button eats up the click event so the label tag + radio won't receive it.
-            // We'll select the radio if it isn't already selected
+            hbnb.showSpecificAmenitiesSubmenu();
             if (!amenRadios[1].checked) {
                 amenRadios[1].click();
             }
         });
 
-        // For the checkboxes in the submenu, let's add events that will update the counter
-        let selectedAmenitiesCheckboxes = document.querySelectorAll("#amenities-submenu >.items input[type='checkbox']");
+        let selectedAmenitiesCheckboxes = document.querySelectorAll("#amenities-submenu > .items input[type='checkbox']");
         for (let c of selectedAmenitiesCheckboxes) {
             c.addEventListener('click', function() {
                 hbnb.updateSpecificAmenitiesCount();
-            })
+            });
         }
 
-        // Last but not least! Now let's add an event to the OK button in the submenu
-        // Note that we are just hiding the menu and doing anything anything special
         let amenSpecificConfirmBtn = document.getElementById("btn-specific-amenities-ok");
-        amenSpecificConfirmBtn.addEventListener('click', function(){
+        amenSpecificConfirmBtn.addEventListener('click', function() {
             hbnb.hideSpecificAmenitiesSubmenu();
-        })
+        });
+    },
+    destinationInit: function() {
+        // set up the onclick events for the Destination radios + button
+        let destRadios = document.querySelectorAll("#menu > .contents > .destination > .choice input[type='radio']");
+        for (let elem of destRadios) {
+            elem.addEventListener("change", function(e) {
+                let specificSelectedText = document.querySelector("#menu > .contents > .destination > .title .selected");
+                let radioValue = e.target.value;
+                if (radioValue == 'specific') {
+                    hbnb.showSpecificDestinationSubmenu();
+                    hbnb.updateSpecificDestinationCount();
+                    specificSelectedText.setAttribute('state', 'show');
+                } else {
+                    hbnb.hideSpecificDestinationSubmenu();
+                    specificSelectedText.setAttribute('state', 'hide');
+                }
+            });
+        }
 
+        let destSpecificSelectBtn = document.getElementById("btn-specific-destination-select");
+        destSpecificSelectBtn.addEventListener('click', function() {
+            hbnb.showSpecificDestinationSubmenu();
+            if (!destRadios[1].checked) {
+                destRadios[1].click();
+            }
+        });
+
+        let selectedDestinationCheckboxes = document.querySelectorAll("#destination-submenu > .items input[type='checkbox']");
+        for (let c of selectedDestinationCheckboxes) {
+            c.addEventListener('click', function() {
+                hbnb.updateSpecificDestinationCount();
+            });
+        }
+
+        let destSpecificConfirmBtn = document.getElementById("btn-specific-destination-ok");
+        destSpecificConfirmBtn.addEventListener('click', function() {
+            hbnb.hideSpecificDestinationSubmenu();
+        });
     },
     showSpecificAmenitiesSubmenu: function() {
-        // I have set up the CSS in a certain way so that the submenu is shown / hidden
-        // depending on the 'state' parameter's value in #amenities-submenu
-        let submenu = document.querySelector("#amenities-submenu")
-        submenu.setAttribute("state", 'show')
+        let submenu = document.querySelector("#amenities-submenu");
+        submenu.setAttribute("state", 'show');
     },
     hideSpecificAmenitiesSubmenu: function() {
-        let submenu = document.querySelector("#amenities-submenu")
-        submenu.setAttribute("state", 'hide')
+        let submenu = document.querySelector("#amenities-submenu");
+        submenu.setAttribute("state", 'hide');
     },
     updateSpecificAmenitiesCount: function() {
-        let specificCount = document.querySelector("#menu >.contents >.amenities >.title .count")
-        let selectedAmenitiesCheckboxes = document.querySelectorAll("#amenities-submenu >.items input[type='checkbox']");
+        let specificCount = document.querySelector("#menu > .contents > .amenities > .title .count");
+        let selectedAmenitiesCheckboxes = document.querySelectorAll("#amenities-submenu > .items input[type='checkbox']");
 
-        let checkedCount = 0
+        let checkedCount = 0;
         for (let c of selectedAmenitiesCheckboxes) {
             if (c.checked) {
-                checkedCount++
+                checkedCount++;
             }
         }
 
-        specificCount.innerHTML = checkedCount
+        specificCount.innerHTML = checkedCount;
+    },
+    showSpecificDestinationSubmenu: function() {
+        let submenu = document.querySelector("#destination-submenu");
+        submenu.setAttribute("state", 'show');
+    },
+    hideSpecificDestinationSubmenu: function() {
+        let submenu = document.querySelector("#destination-submenu");
+        submenu.setAttribute("state", 'hide');
+    },
+    updateSpecificDestinationCount: function() {
+        let specificCount = document.querySelector("#menu > .contents > .destination > .title .count");
+        let selectedDestinationCheckboxes = document.querySelectorAll("#destination-submenu > .items input[type='checkbox']");
+
+        let checkedCount = 0;
+        for (let c of selectedDestinationCheckboxes) {
+            if (c.checked) {
+                checkedCount++;
+            }
+        }
+
+        specificCount.innerHTML = checkedCount;
     },
     init: function() {
         hbnb.amenitiesInit();
+        hbnb.destinationInit();
     }
 }
 
 window.onload = function() {
-    // We add something to the web site to indicate that JS is active
-    // otherwise a big scary message will appear
     let body = document.getElementsByTagName("body")[0];
     body.setAttribute("js", "ok");
 
