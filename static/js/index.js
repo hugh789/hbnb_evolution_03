4fe6bbb38f1d2932fc956cbe07d856afa4f4a680
root <hugh.fagan123@gmail.com>
2024-08-13 15:43:32 +1000

    Updated inner html on country filterresults by country javascript to render Country and City so far, places is throwing error atm

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,19 @@
-hbnb = {
+const hbnb = {
+    init: function() {
+        // Initialize amenities and destination functionality
+        this.amenitiesInit();
+        this.destinationInit();
+        this.destinationSelectInit();
+    },
+    
     amenitiesInit: function() {
-        // set up the onclick events for the Amenities radios + button
-        let amenRadios = document.querySelectorAll("#menu > .contents > .amenities > .choice input[type='radio']");
-        for (let elem of amenRadios) {
+        // Set up the onclick events for the Amenities radios + button
+        const amenRadios = document.querySelectorAll("#menu > .contents > .amenities > .choice input[type='radio']");
+        for (const elem of amenRadios) {
             elem.addEventListener("change", function(e) {
-                let specificSelectedText = document.querySelector("#menu > .contents > .amenities > .title .selected");
-                let radioValue = e.target.value;
-                if (radioValue == 'specific') {
+                const specificSelectedText = document.querySelector("#menu > .contents > .amenities > .title .selected");
+                const radioValue = e.target.value;
+                if (radioValue === 'specific') {
                     hbnb.showSpecificAmenitiesSubmenu();
                     hbnb.updateSpecificAmenitiesCount();
                     specificSelectedText.setAttribute('state', 'show');
@@ -17,7 +24,7 @@ hbnb = {
             });
         }
 
-        let amenSpecificSelectBtn = document.getElementById("btn-specific-amenities-select");
+        const amenSpecificSelectBtn = document.getElementById("btn-specific-amenities-select");
         amenSpecificSelectBtn.addEventListener('click', function() {
             hbnb.showSpecificAmenitiesSubmenu();
             if (!amenRadios[1].checked) {
@@ -25,27 +32,27 @@ hbnb = {
             }
         });
 
-        let selectedAmenitiesCheckboxes = document.querySelectorAll("#amenities-submenu > .items input[type='checkbox']");
-        for (let c of selectedAmenitiesCheckboxes) {
+        const selectedAmenitiesCheckboxes = document.querySelectorAll("#amenities-submenu > .items input[type='checkbox']");
+        for (const c of selectedAmenitiesCheckboxes) {
             c.addEventListener('click', function() {
                 hbnb.updateSpecificAmenitiesCount();
             });
         }
 
-        let amenSpecificConfirmBtn = document.getElementById("btn-specific-amenities-ok");
+        const amenSpecificConfirmBtn = document.getElementById("btn-specific-amenities-ok");
         amenSpecificConfirmBtn.addEventListener('click', function() {
             hbnb.hideSpecificAmenitiesSubmenu();
         });
     },
 
     destinationInit: function() {
-        // set up the onclick events for the Destination radios + button
-        let destRadios = document.querySelectorAll("#menu > .contents > .destination > .choice input[type='radio']");
-        for (let elem of destRadios) {
+        // Set up the onclick events for the Destination radios + button
+        const destRadios = document.querySelectorAll("#menu > .contents > .destination > .choice input[type='radio']");
+        for (const elem of destRadios) {
             elem.addEventListener("change", function(e) {
-                let specificSelectedText = document.querySelector("#menu > .contents > .destination > .title .selected");
-                let radioValue = e.target.value;
-                if (radioValue == 'specific') {
+                const specificSelectedText = document.querySelector("#menu > .contents > .destination > .title .selected");
+                const radioValue = e.target.value;
+                if (radioValue === 'specific') {
                     hbnb.showSpecificDestinationSubmenu();
                     hbnb.updateSpecificDestinationCount();
                     specificSelectedText.setAttribute('state', 'show');
@@ -56,8 +63,8 @@ hbnb = {
             });
         }
 
-        let selectedDestinationCheckboxes = document.querySelectorAll("#destination-submenu > .items input[type='checkbox']");
-        for (let c of selectedDestinationCheckboxes) {
+        const selectedDestinationCheckboxes = document.querySelectorAll("#destination-submenu > .items input[type='checkbox']");
+        for (const c of selectedDestinationCheckboxes) {
             c.addEventListener('click', function() {
                 hbnb.updateSpecificDestinationCount();
             });
@@ -65,21 +72,21 @@ hbnb = {
     },
 
     showSpecificAmenitiesSubmenu: function() {
-        let submenu = document.querySelector("#amenities-submenu");
+        const submenu = document.querySelector("#amenities-submenu");
         submenu.setAttribute("state", 'show');
     },
 
     hideSpecificAmenitiesSubmenu: function() {
-        let submenu = document.querySelector("#amenities-submenu");
+        const submenu = document.querySelector("#amenities-submenu");
         submenu.setAttribute("state", 'hide');
     },
 
     updateSpecificAmenitiesCount: function() {
-        let specificCount = document.querySelector("#menu > .contents > .amenities > .title .count");
-        let selectedAmenitiesCheckboxes = document.querySelectorAll("#amenities-submenu > .items input[type='checkbox']");
+        const specificCount = document.querySelector("#menu > .contents > .amenities > .title .count");
+        const selectedAmenitiesCheckboxes = document.querySelectorAll("#amenities-submenu > .items input[type='checkbox']");
 
         let checkedCount = 0;
-        for (let c of selectedAmenitiesCheckboxes) {
+        for (const c of selectedAmenitiesCheckboxes) {
             if (c.checked) {
                 checkedCount++;
             }
@@ -89,21 +96,21 @@ hbnb = {
     },
 
     showSpecificDestinationSubmenu: function() {
-        let submenu = document.querySelector("#destination-submenu");
+        const submenu = document.querySelector("#destination-submenu");
         submenu.setAttribute("state", 'show');
     },
 
     hideSpecificDestinationSubmenu: function() {
-        let submenu = document.querySelector("#destination-submenu");
+        const submenu = document.querySelector("#destination-submenu");
         submenu.setAttribute("state", 'hide');
     },
 
     updateSpecificDestinationCount: function() {
-        let specificCount = document.querySelector("#menu > .contents > .destination > .title .count");
-        let selectedDestinationCheckboxes = document.querySelectorAll("#destination-submenu > .items input[type='checkbox']");
+        const specificCount = document.querySelector("#menu > .contents > .destination > .title .count");
+        const selectedDestinationCheckboxes = document.querySelectorAll("#destination-submenu > .items input[type='checkbox']");
 
         let checkedCount = 0;
-        for (let c of selectedDestinationCheckboxes) {
+        for (const c of selectedDestinationCheckboxes) {
             if (c.checked) {
                 checkedCount++;
             }
@@ -146,24 +153,70 @@ hbnb = {
         })
         .then(data => {
             console.log(data);
-            let results = "";
+            let results = "<ul class='listings'>";
+
             for (let countryName in data) {
                 if (data.hasOwnProperty(countryName)) {
                     const countryData = data[countryName];
-                    results += "<div> \
-                        <h1>" + countryName + "</h1> \
-                        <div>Stuff goes here</div> \
-                    </div>";
+                    results += "<li class='country_group'>" +
+                                    "<div class='country_name'>" +
+                                        "<h1>" + countryName + "</h1>" +
+                                    "</div>" +
+                                    "<ul class='cities'>";
+
+                    for (let cityName in countryData) {
+                        if (countryData.hasOwnProperty(cityName)) {
+                            const cityPlaces = countryData[cityName];
+                            results += "<li class='city_group'>" +
+                                            "<div class='city_name'>" + cityName + "</div>" +
+                                            "<ul>";
+
+               /*              for (const place of cityPlaces) { // Assuming cityPlaces is an array
+                                results += "<li class='place'>" +
+                                                "<img src='static/img/malaysia.jpg' alt='place-img' style='width:100%' class='test-img'>" +
+                                                "<div class='name'>" + place.name + "</div>" +
+                                                "<div class='details'>" +
+                                                    "<div class='address' latitude='" + place.latitude + "' longitude='" + place.longitude + "'>" +
+                                                        "<span class='text'>" + (place.address ? place.address.substring(0, 25) + '...' : '') + "</span>" + 
+                                                    "</div>" +
+                                                    "<div class='price'>" +
+                                                        "<div class='digits'></div>" +
+                                                        "<strong class='price_per_night'>$" + place.price_per_night + "</strong>" +
+                                                        "<div class='pax'>per night</div>" +
+                                                    "</div>" +
+                                                    "<div class='accommodations'>" +
+                                                        "<span class='rooms'><i class='fa-solid fa-house'></i>" + place.number_of_rooms + "</span>" +
+                                                        "<span class='bathrooms'><i class='fa-solid fa-bath'></i>" + place.number_of_bathrooms + "</span>" +
+                                                        "<span class='guests'><i class='fa-solid fa-person'></i>" + place.max_guests + "</span>" +
+                                                    "</div>" +
+                                                "</div>" +
+                                            "</li>";
+                            }
+ */
+                            results += "</ul>" +
+                                    "</li>";
+                        }
+                    }
+
+                    results += "</ul>" +
+                            "</li>";
                 }
             }
-            document.querySelector("#results").innerHTML = results;
+
+            results += "</ul>";
+
+            document.querySelector("#results-container").innerHTML = results;
         })
         .catch(error => console.error("Error fetching places:", error));
+    },
+
+    filterResultsByCity: function(selectedCity) {
+        // This function should be implemented to handle filtering by city if needed
     }
 };
 
 window.onload = function() {
-    let body = document.getElementsByTagName("body")[0];
+    const body = document.getElementsByTagName("body")[0];
     body.setAttribute("js", "ok");
 
     hbnb.init();
